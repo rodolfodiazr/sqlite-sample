0fea63ee630bdb7777c6a71154abcb426fd7bbe8
Rodolfo Diaz <rdiaz@wawand.co>
2018-11-26 17:28:32 -0500

    Added a new RecyclerView.

--- app/src/main/java/com/example/databasetesting/adapters/ElementsAdapter.java
+++ app/src/main/java/com/example/databasetesting/adapters/ElementsAdapter.java
@@ -1,26 +1,52 @@
 package com.example.databasetesting.adapters;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.databasetesting.R;
 
 /**
  * Created by rdiaz on 11/26/18.
  */
 public class ElementsAdapter extends RecyclerView.Adapter {
+
+	private Context mContext;
+
+	public ElementsAdapter(Context context) {
+		mContext = context;
+	}
+
 	@NonNull
 	@Override
-	public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-		return null;
+	public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+		View view = LayoutInflater.from(parent.getContext())
+				.inflate(R.layout.item_list, parent, false);
+		RecyclerView.ViewHolder viewHolder = new ElementViewHolder(view);
+		return viewHolder;
 	}
 
 	@Override
-	public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
-
+	public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+		((ElementViewHolder) holder).mName.setText("Sample");
 	}
 
 	@Override
 	public int getItemCount() {
 		return 0;
+	}
+
+	private class ElementViewHolder extends RecyclerView.ViewHolder {
+
+		private final TextView mName;
+
+		public ElementViewHolder(View view) {
+			super(view);
+			mName = view.findViewById(R.id.elementNameTextView);
+		}
 	}
 }
